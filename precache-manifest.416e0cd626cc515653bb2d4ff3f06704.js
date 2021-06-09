fb55804cbf9208ef3183acb6d100a38f247f17f9
Alex Lau <lauyy@umich.edu>
2021-06-09 20:08:35 +0800

    okkkk

---
+++ precache-manifest.416e0cd626cc515653bb2d4ff3f06704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e1ce95bf843b72a9881151256b0f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f61a5d282482f63025",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "18b54f6a1ff06ece9ed2",
+    "url": "/static/css/main.314f5e49.chunk.css"
+  },
+  {
+    "revision": "88f61a5d282482f63025",
+    "url": "/static/js/2.3d4b7d1f.chunk.js"
+  },
+  {
+    "revision": "18b54f6a1ff06ece9ed2",
+    "url": "/static/js/main.4451bb63.chunk.js"
+  },
+  {
+    "revision": "92c89455b99cd109be4a",
+    "url": "/static/js/runtime-main.b5d1efea.js"
+  }
+]);
