40a747a783f331b3a1cdcd4d660be0d08ae38fd2
Alex Lau <lauyy@umich.edu>
2021-06-11 11:10:51 +0800

    add missing files

---
+++ precache-manifest.9ba6fbea7645294467b36fb3df27eb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d74d8e141c232df375481e34334920a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c7b26e4eda957514246",
+    "url": "./static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "75c35083c75c506c668c",
+    "url": "./static/css/main.314f5e49.chunk.css"
+  },
+  {
+    "revision": "3c7b26e4eda957514246",
+    "url": "./static/js/2.2ac23bfe.chunk.js"
+  },
+  {
+    "revision": "b1c5561c2231ef270f3b081aaeb74ccd",
+    "url": "./static/js/2.2ac23bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c35083c75c506c668c",
+    "url": "./static/js/main.614577e2.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
